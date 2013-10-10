c8c991c26a34c0febf9e0c915530016258625067
DetaX <detax.64@gmail.com>
2013-10-10 14:20:37 +0200

    adding nbdisk parameter to cylinder

--- objects/cylinder.cpp
+++ objects/cylinder.cpp
@@ -1,30 +1,32 @@
 #include "cylinder.h"
 
-Cylinder::Cylinder(int nvertices)
+Cylinder::Cylinder(int nvertices, int ndisk)
 {
     _name="Cylinder";
-    // origin of top disk
-    this->addVertex(0,0,1);
-    for (float i=0; i<2*M_PI ; i+=(2/(float)nvertices)*M_PI)
-        this->addVertex(cos(i),sin(i),1);
-
-    // origin of bot disk
-        this->addVertex(0,0,-1);
-    for (float i=0; i<2*M_PI ; i+=(2/(float)nvertices)*M_PI)
-        this->addVertex(cos(i),sin(i),-1);
+    //vertices
+    for (int i=0;i<ndisk;++i) {
+        float z = 1-i*2/(float(ndisk)-1);
+        this->addVertex(0,0,z);
+        for (float i=0; i<2*M_PI ; i+=(2/(float)nvertices)*M_PI)
+            this->addVertex(cos(i),sin(i),z);
+    }
 
     //top disk
-    for (int j=0; j<nvertices+1; ++j)
-      this->addTriangle(j,0,j%nvertices+1);
-    //bot disk
+    for (int j=1; j<nvertices+1; ++j)
+        this->addTriangle(0,j,j%nvertices+1);
+    //bottom disk
     for (int j=0; j<nvertices; ++j)
-      this->addTriangle(j+nvertices+2,nvertices+1,(j+1)%nvertices+2+nvertices);
+        this->addTriangle(j+(ndisk-1)*nvertices+ndisk,(ndisk-1)*(nvertices+1),(j+1)%nvertices+ndisk+(ndisk-1)*nvertices);
+
 
     //side triangles
-    for (int j=1; j<nvertices+1; ++j)
-      this->addTriangle(1+j%nvertices,j,j+1+nvertices);
-    for (int j=1; j<nvertices+1; ++j)
-      this->addTriangle(j%nvertices+2+nvertices,j%nvertices+1,nvertices+j+1);
+    for(int i=0;i<ndisk-1;++i) {
+        for (int j=1; j<nvertices+1; ++j)
+            this->addTriangle(1+j%nvertices+i*(nvertices+1),j+i*(nvertices+1),j+1+nvertices+i*(nvertices+1));
+       for (int j=1; j<nvertices+1; ++j)
+          this->addTriangle(j%nvertices+2+nvertices+i*(nvertices+1),j%nvertices+1+i*(nvertices+1),nvertices+j+1+i*(nvertices+1));
+
+    }
 
     computeNormalsT();
     computeNormalsV();
